7d55a15030f2c6c03b8d9ba4b9857ca84bbde47f
ddf9795 <89398656+ddf9795@users.noreply.github.com>
2024-12-01 18:19:55 -0500

    fixing

--- server/router.js
+++ server/router.js
@@ -4,7 +4,7 @@ const mid = require('./middleware');
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, 'uploads');
+    cb(null, 'public/uploads');
   },
   filename: (req, file, cb) => {
     cb(null, `${file.fieldname}-${Date.now()}`);
